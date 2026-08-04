6574051b76ea0bcfa8169d1f02abd878ba1bb412
Jinghao Hu <731409213@qq.com>
2026-08-05 07:48:17 +0800

    Expose joint GEO API

--- src/spatialshap/__init__.py
+++ src/spatialshap/__init__.py
@@ -2,6 +2,9 @@
 
 from spatialshap._explainer import Explainer, explain
 from spatialshap._explanation import SpatialExplanation
+from spatialshap._geo_coalitions import GeoDecompositionDiagnostics
+from spatialshap._geo_explainer import GeoExplainer, explain_geo
+from spatialshap._geo_explanation import GeoExplanation
 from spatialshap._references import (
     GlobalReference,
     KernelReference,
@@ -16,10 +19,14 @@ __license__ = "MIT"
 __all__ = [
     "Explainer",
     "SpatialExplanation",
+    "GeoExplainer",
+    "GeoExplanation",
+    "GeoDecompositionDiagnostics",
     "GlobalReference",
     "KernelReference",
     "KNNReference",
     "ReferenceDiagnostics",
     "explain",
+    "explain_geo",
     "__version__",
 ]
